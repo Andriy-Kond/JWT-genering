6913181def40e8e451961dfaed710d8b628e7654
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-06-26 10:45:20 +0300

    ADD:  jwt.verify

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ console.log('decodeToken:', decodeToken);
 // decodeToken: { id: '647b4930bf825983daba5742', iat: 1685801778, exp: 1685884578 }
 // iat - час коли токен був створений
 // exp - час коли токен закінчиться
+// Або так:
+const verifyToken = jwt.verify(token, SECRET_KEY);
+console.log('verifyToken:', verifyToken);
+// verifyToken: { id: '647b4930bf825983daba5742', iat: 1687765354, exp: 1687848154 }
 
 // Будь-хто може розкодувати токен, тому у payload персональні дані не зберігаються.
 
@@ -29,9 +33,11 @@ console.log('decodeToken:', decodeToken);
 // Цей метод перевіряє чи шифрували ми його за допомогою цього секретного ключа і чи не закінчився у нього термін дії.
 // Якщо це невірний токен, то verify кидає помилку. Він не повертає false. Тому ми його загортаємо у try...catch
 // Якщо токен вірний, то verify поверне payload, в якому ми можемо деструктуризувати id:
+
 try {
   const { id } = jwt.verify(token, SECRET_KEY);
   console.log('id:', id);
+  // id: 647b4930bf825983daba5742
 
   const fakeToken =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0N2I0OTMwYmY4MjU5ODNkYWJhNTc0MiIsImlhdCI6MTY4NTgwMTY0NywiZXhwIjoxNjg1ODg0NDQ3fQ.QpZgwyFF49NI_3pawFIQfbeGf0uHH7r-uGfSHxvrEBo';
